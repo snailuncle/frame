3c8216ae5cb7df8f91f27e8e40ed15b2c82c08fd
snailuncle <1789500304@qq.com>
2019-05-27 00:35:03 +0800

    5-27-添加是否强制更新common参数

--- common.js
+++ common.js
@@ -1,20 +1,29 @@
 //  //导入模块
-//  function 导入常用函数模块() {
-//    var url = 'https://raw.githubusercontent.com/snailuncle/frame/master/common.js'
-//    var r = http.get(url)
-//    log("code = " + r.statusCode);
-//    var html = r.body.bytes()
-//    var commonFunctionPath = './common.js'
-//    files.write(commonFunctionPath, '')
-//    files.writeBytes(commonFunctionPath, html)
-//    var common = require(commonFunctionPath)
-//    return common
-//  }
-//  var common = 导入常用函数模块()
-//  log(common)
-//  for (let i = 0; i < 33; i++) {
-//    common.flash('fire in the hole')
-//  }
+//  function 导入常用函数模块(是否强制重新下载common) {
+//   var 是否强制重新下载common = 是否强制重新下载common || false
+//   var commonFunctionPath = './common.js'
+
+//   function downloadCommon() {
+//     var url = 'https://raw.githubusercontent.com/snailuncle/frame/master/common.js'
+//     var r = http.get(url)
+//     var html = r.body.bytes()
+//     files.write(commonFunctionPath, '')
+//     files.writeBytes(commonFunctionPath, html)
+//   }
+//   if (是否强制重新下载common) {
+//     downloadCommon()
+//   }
+//   if (files.exists(commonFunctionPath)) {} else {
+//     downloadCommon()
+//   }
+//   var common = require(commonFunctionPath)
+//   return common
+// }
+// var common = 导入常用函数模块()
+// for (let i = 0; i < 33; i++) {
+//   common.flash('fire in the hole')
+// }
+
 
 var 是否打印日志 = false
 
